7368846a49862b1780be2ef5a8507c7eed4ab8cc
Sejo170 <josehernandopardos@gmail.com>
2026-01-26 12:07:42 +0100

    26 de enero

--- first-steps/src/app/app-module.ts
+++ first-steps/src/app/app-module.ts
@@ -10,6 +10,7 @@ import { Home } from './components/home/home';
 import { Info } from './components/info/info';
 import { Navbar } from './components/navbar/navbar';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- first-steps/src/app/components/info/info.ts
+++ first-steps/src/app/components/info/info.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RickMorty } from '../../services/rick-morty';
+import { CharacterRM, InfoApiRM } from '../../common/rminterface';
 
 @Component({
   selector: 'app-info',
@@ -6,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './info.html',
   styleUrl: './info.css',
 })
-export class Info {
+export class Info implements OnInit {
+  filterByName() {
+    throw new Error('Method not implemented.');
+  }
+  dataApi!: InfoApiRM;
+  characters: CharacterRM[] = [];
+  activeIndex: number | null = null;
+  charName: string = " ";
 
+  constructor(private rmservice: RickMorty) { }
+  
+  ngOnInit(): void {
+    this.loadCharacters();
+  }
+
+  private loadCharacters() {
+    this.rmservice.getCharacters().subscribe({
+      next: value => {
+        this.dataApi = value;
+        this.characters = this.dataApi.results;
+      },
+      error: err => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log("Done");
+      }
+    });
+  }
+  toggleAccordion(index:number) {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
 }
